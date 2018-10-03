b195a6063e3fa1bc8f85b27caf518c9e5a77937a
Kumar Abhishek <kr.abhish@gmail.com>
2018-10-03 10:21:36 -0700

    Update stl.cpp

--- stl.cpp
+++ stl.cpp
@@ -322,3 +322,22 @@ for (size_t i = 1; i < b.size(); i += 2) {
 }
 std::cout << b << " "<< b.to_ulong()<<'\n'; // similarly we have to_string() etc.
 //Output: 10101010 170
+
+        
+//file I/O
+char str[10];
+//Creates an instance of ofstream, and opens example.txt
+ofstream a_file ( "example.txt" );
+// Outputs to example.txt through a_file
+a_file<<"This text will now be inside of example.txt";
+// Close the file stream explicitly
+a_file.close();
+        
+//Opens for reading the file
+ifstream b_file ( "example.txt" );
+//Reads one string from the file
+b_file>> str;
+//Should output 'this'
+cout<< str <<"\n";
+cin.get();    // wait for a keypress
+// b_file is closed implicitly here
