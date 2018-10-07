0298fe603ceb6b0ce5c9011f85ca8b89dedec13f
Kumar Abhishek <kr.abhish@gmail.com>
2018-10-06 22:32:17 -0700

    Update regex.cpp

--- regex.cpp
+++ regex.cpp
@@ -12,3 +12,22 @@ bool isMatchRec(string s, string p, int i, int j){
         return firstMatch && isMatchRec(s, p, i+1, j+1);
     }
 }
+
+//top down memoization
+bool isMatchRec(string s, string p, int i, int j, vector<vector<int>> &dp) {
+    if(j==p.size()) return i==s.size();
+    if(dp[i][j]!=-1)  return dp[i][j];
+    bool firstMatch = (i<s.size()) && (s[i] == p[j] || (p[j] == '.'));
+    bool ans;
+    if(j+1< p.size() && p[j+1] == '*') {
+        ans = isMatchRec(s, p, i, j+2, dp) || (firstMatch && isMatchRec(s, p, i+1, j, dp));
+    }
+    else {
+        ans = firstMatch && isMatchRec(s, p, i+1, j+1, dp);
+    }
+    dp[i][j] = ans;
+    return ans;
+}
+vector<vector<int>> dp(m+1, vector<int>(n+1,-1));        
+
+
