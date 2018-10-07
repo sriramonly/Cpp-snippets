40952affde5c752340da35ef029a66abad12b5bf
Kumar Abhishek <kr.abhish@gmail.com>
2018-10-06 23:24:58 -0700

    Update regex.cpp

--- regex.cpp
+++ regex.cpp
@@ -31,3 +31,21 @@ bool isMatchRec(string s, string p, int i, int j, vector<vector<int>> &dp) {
 vector<vector<int>> dp(m+1, vector<int>(n+1,-1));        
 
 
+//bottom up dp
+bool isMatchRec(string s, string p, vector<vector<bool>> &dp) {
+    dp[m][n] = true;
+    for(int i=m;i>=0;i--) {
+        for(int j=n-1;j>=0;j--) {
+            bool firstMatch = (i<s.size()) && (s[i] == p[j] || (p[j] == '.'));
+            bool ans;
+            if(j+1< p.size() && p[j+1] == '*') {
+                ans = dp[i][j+2] || (firstMatch && dp[i+1][j]);
+            }
+            else {
+                ans = firstMatch && dp[i+1][j+1];
+            }
+            dp[i][j] = ans;
+        }
+    }
+    return dp[0][0];
+}
