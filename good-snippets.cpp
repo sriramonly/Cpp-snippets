1d2ece1efc07b353bc702177d3b2dbc40e9461b9
Kumar Abhishek <kr.abhish@gmail.com>
2018-10-31 21:40:25 -0700

    Update good-snippets.cpp

--- good-snippets.cpp
+++ good-snippets.cpp
@@ -5,3 +5,14 @@ int isHeightBalancedRec(TreeNode* root, bool &isbal) {
         if(abs(l-r) > 1) isbal = false;
         return 1+ max(l, r);
 }
+
+//Move all zeroes to end of list
+void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]!=0) {
+                swap(nums[i], nums[j]);
+                ++j;
+            }
+        }
+}
